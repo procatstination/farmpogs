395b2af0ed6aa3ae34af2276a9f8d7f71ebfabaf
procatstination <lovingprocatsination@gmail.com>
2021-03-30 20:19:14 +0100

    gather script with just vodid

--- gatherContent.sh
+++ gatherContent.sh
@@ -15,14 +15,13 @@ mkdir ./clips/$vodID
 
 cd ./out/$vodID
 echo 'Downloading chat log'
-tcd --channel hasanabi --video $vodID --format irc --output ./ --first=1 
+tcd --video $vodID --format irc --output ./ --first=1 
 
 #Rename chat file
 mv *.log chat.log
 
-
 echo 'Downloading vod'
-twitch-dl download $vodID --overwrite -q 720p60 #not using source to speed up download
+twitch-dl download $vodID --overwrite -q source #not using source to speed up download
 # get title and rename
 
 #Rename vod file
