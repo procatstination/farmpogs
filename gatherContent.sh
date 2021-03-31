5674b00812f0588207ff0e97534ec0ec788c032b
procatstination <lovingprocatsination@gmail.com>
2021-03-31 14:13:34 +0100

    removing trim time for download as should perform with the python script

--- gatherContent.sh
+++ gatherContent.sh
@@ -6,8 +6,6 @@ do
     case "${flag}" in
         v) vodID=${OPTARG} ;;
         q) quality=${OPTARG} ;;
-        s) start=${OPTARG} ;;
-        e) end=${OPTARG} ;;
     esac
 done
 
@@ -19,10 +17,10 @@ echo 'Downloading chat log'
 tcd --video $vodID --format irc --output ./ --first=1 
 
 #Rename chat file
-mv *.log chat.log
+#mv *.log chat.log
 
 echo 'Downloading vod'
-twitch-dl download $vodID --quality $quality --start $start --end $end #not using source to speed up download
+twitch-dl download $vodID --quality $quality
 # get title and rename
 
 #Rename vod file
